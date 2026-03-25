36af37246bbcc0062878a8629763d77578ae81e5
bdaqiq01 <bdaqiq01@gmail.com>
2026-03-24 23:57:46 -0700

    adding he4

---
+++ conts_trans/build/CMakeFiles/HU.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HU.dir/HU_lines.cpp.o"
+  "CMakeFiles/HU.dir/HU_lines.cpp.o.d"
+  "HU"
+  "HU.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
