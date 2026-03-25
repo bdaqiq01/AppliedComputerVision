a610135e4c41dca2c8979ba87aaccdb6fa21372d
bdaqiq01 <bdaqiq01@gmail.com>
2026-03-25 12:21:02 -0700

    hlines working

--- conts_trans/HU_lines.cpp
+++ conts_trans/HU_lines.cpp
@@ -30,7 +30,8 @@ int main(int argc, char** argv)
     namedWindow("Capture Example");
     Mat frame;
     Mat gray, canny_frame, cdst;
-    vector<Vec4i> lines;
+   // vector<Vec4i> lines; //FOR highInes p
+    vector<Vec2f> lines; //for lines
     
     if(argc < 2) //parsing the command line i
     {
@@ -58,7 +59,7 @@ int main(int argc, char** argv)
     else if (strcmp(option, "video") == 0) //open the video into vcap
     
     {
-    	if(!vcap.open("GOPR0639.MP4")) 
+    	if(!vcap.open("22400003.mp4")) 
 	    {
 		std::cout << "Error opening video or file" << std::endl;
 		return -1;
@@ -88,15 +89,32 @@ int main(int argc, char** argv)
         cvtColor(frame, gray, COLOR_BGR2GRAY);
 
         // Probabilistic Line Transform
-        HoughLinesP(canny_frame, lines, 1, CV_PI/180, 50, 50, 10);
+        //HoughLinesP(canny_frame, lines, 1, CV_PI/180, 50, 50, 10);
 
-        for( size_t i = 0; i < lines.size(); i++ )
-        {
-          Vec4i l = lines[i];
-          line(cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, LINE_AA);
-        }
+            // Standard Hough Line Transform
+	    HoughLines(canny_frame, lines, 1, CV_PI/180, 200, 0, 0 ); // runs the actual detection
+	    // Draw the lines
+	    for( size_t i = 0; i < lines.size(); i++ )
+	    {
+	        float rho = lines[i][0], theta = lines[i][1];
+	        Point pt1, pt2;
+	        double a = cos(theta), b = sin(theta);
+	        double x0 = a*rho, y0 = b*rho;
+	        pt1.x = cvRound(x0 + 1000*(-b));
+	        pt1.y = cvRound(y0 + 1000*(a));
+	        pt2.x = cvRound(x0 - 1000*(-b));
+	        pt2.y = cvRound(y0 - 1000*(a));
+	        line( cdst, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
+	    }
 
-//*/
+
+//        for( size_t i = 0; i < lines.size(); i++ ) //f9r hlinep only 
+//        {
+//          Vec4i l = lines[i];
+//          line(cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, LINE_AA);
+ //       }
+
+
         //imshow("Capture Example", frame);
 
         imshow("Capture Example", cdst);
---
+++ conts_trans/build/CMakeFiles/skel_transform.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skel_transform.dir/skel_transform.cpp.o"
+  "CMakeFiles/skel_transform.dir/skel_transform.cpp.o.d"
+  "skel_transform"
+  "skel_transform.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skel_transform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
