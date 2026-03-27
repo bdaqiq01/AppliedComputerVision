b531d13d9a481ddfc0e6e50c6a650d7bd5f88903
bdaqiq01 <bdaqiq01@gmail.com>
2026-03-27 12:06:07 -0700

    Updated homework files; removed video

---
+++ conts_trans/build/CMakeFiles/people_detec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/people_detec.dir/people_detec.cpp.o"
+  "CMakeFiles/people_detec.dir/people_detec.cpp.o.d"
+  "people_detec"
+  "people_detec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/people_detec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
